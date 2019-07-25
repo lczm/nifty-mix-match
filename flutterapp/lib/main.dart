fa893fc8840528fd35af09baeac225d85875038c
ossaris56 <ossaris56@gmail.com>
2019-07-25 18:35:28 +0800

    not working card swipe

--- flutterapp/lib/main.dart
+++ flutterapp/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:fluttery/layout.dart';
 
+import 'cards.dart';
+
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
@@ -27,6 +29,7 @@ class _MyHomePageState extends State<MyHomePage> {
     return new AppBar(
       backgroundColor: Colors.transparent,
       elevation: 0.0,
+      centerTitle: true,
       leading: new IconButton(
         icon: new Icon(
           Icons.person,
@@ -106,127 +109,16 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  Widget _buildCardStack() {
-    return new AnchoredOverlay(
-      showOverlay: true,
-      child: new Center(),
-      overlayBuilder: (BuildContext context, Rect anchorBounds, Offset anchor) {
-        return CenterAbout(
-          position: anchor,
-          child: new Container(
-            width: anchorBounds.width,
-            height: anchorBounds.height,
-            padding: const EdgeInsets.all(16.0),
-            child: new ProfileCard(),
-          ),
-        );
-      },
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: _buildAppBar(),
-      body: _buildCardStack(),
+      body: new DraggableCard(),
       bottomNavigationBar: _buildBottomBar(),
     );
   }
 }
 
-class ProfileCard extends StatefulWidget {
-  @override
-  _ProfileCardState createState() => new _ProfileCardState();
-}
-
-class _ProfileCardState extends State<ProfileCard> {
-  Widget _buildBackground() {
-    return new Image.asset(
-      'assets/testphoto.jpg',
-      fit: BoxFit.cover,
-    );
-  }
-
-  Widget _buildProfileSynopsis() {
-    return new Positioned(
-      left: 0.0,
-      right: 0.0,
-      bottom: 0.0,
-      child: new Container(
-        decoration: new BoxDecoration(
-          gradient: new LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [
-              Colors.transparent,
-              Colors.black.withOpacity(0.8),
-            ],
-          ),
-        ),
-        padding: const EdgeInsets.all(24.0),
-        child: new Row(
-          mainAxisSize: MainAxisSize.max,
-          children: <Widget>[
-            new Expanded(
-              child: new Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  new Text(
-                    'First Last',
-                    style: new TextStyle(
-                      color: Colors.white,
-                      fontSize: 24.0,
-                    ),
-                  ),
-                  new Text(
-                    'Some description',
-                    style: new TextStyle(
-                      color: Colors.white,
-                      fontSize: 18.0,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            new Icon(
-              Icons.info,
-              color: Colors.white,
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new Container(
-      decoration: new BoxDecoration(
-        borderRadius: new BorderRadius.circular(10.0),
-        boxShadow: [
-          new BoxShadow(
-            color: const Color(0x11000000),
-            blurRadius: 5.0,
-            spreadRadius: 2.0,
-          ),
-        ],
-      ),
-      child: ClipRRect(
-        borderRadius: new BorderRadius.circular(10.0),
-        child: new Material(
-          child: new Stack(
-            fit: StackFit.expand,
-            children: <Widget>[
-              _buildBackground(),
-              _buildProfileSynopsis(),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
 
 class RoundIconButton extends StatelessWidget {
   final IconData icon;
